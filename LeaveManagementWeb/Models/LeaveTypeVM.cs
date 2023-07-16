d6485cf41a286b82909649a12098a943a837b884
Andrew Buteskul <buteskul5.6@gmail.com>
2023-07-16 22:07:49 +0300

    Added datatable, upgrated CRUD screens, configured Automapper.

---
+++ LeaveManagementWeb/Models/LeaveTypeVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementWeb.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Quantity of days by default")]
+        [Required]
+        [Range(1,24, ErrorMessage = "Please, fill in a valid range")]
+        public int DefaultDays { get; set; }
+
+    }
+}
